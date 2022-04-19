0d7b04e212fa54ddd55959beff5f8ad2996450d7
Vishal Kumar Singh <vishalsingh8574031285@gmail.com>
2022-04-19 06:47:36 +0530

    First Assignment Completed

---
+++ main.js
@@ -0,0 +1,6 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+console.log(add(5, 9), subtract(15, 9), multiply(4, 5), divide(9, 3));
---
+++ subtract.js
@@ -0,0 +1,5 @@
+const subtrsct = (a, b) => {
+    return a - b;
+}
+
+module.exports = subtrsct;
